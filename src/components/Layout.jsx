398a5abccd01b92b251e378ccb87de5adb28b71d
Theolin Nadasen <nadasentheolin@gmail.com>
2025-05-21 05:20:19 +0200

    changed content on pages

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,14 +3,16 @@ import { useNavigate } from 'react-router-dom'
 import { Button } from '@mui/material';
 
 
-const Layout = ({ page, pageName, children }) => {
+const Layout = ({ page, pageName, title='page', children }) => {
     const navigate = useNavigate();
 
     return (
         <div className='h-screen bg-amber-50 flex justify-center items-center flex-col'>
-            <div className='w-3/4 h-1/2 md:w-1/4 md:h-2/3 bg-[#F1F0F0] mb-5 container flex items-center flex-col gap-5'>
-                <h1 className='bg-red-500 w-full h-10 text-center pt-1 font-[fantasy] text-2xl rounded-t-lg'>Title</h1>
+            <div className='w-3/4 h-1/2 md:w-1/4 md:h-2/3 bg-[#F1F0F0] mb-5 container flex items-center flex-col'>
+                <h1 className='bg-red-500 w-full h-10 text-center pt-1 font-[exile] text-2xl rounded-t-lg'>{title}</h1>
+
                 {children}
+
             </div>
 
             <div className='flex flex-row gap-5'>
--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -7,10 +7,28 @@ import Layout from '../components/Layout'
 function Home() {
 
   return (
-    <Layout page={'/about'} pageName={'About'}>
-      <h2>some text</h2>
-      <h2>some text</h2>
-      <h2>some text</h2>
+    <Layout page={'/about'} pageName={'About'} title='Home'>
+
+      <div className='flex flex-col justify-between w-full h-full'>
+
+        <div>
+          <div className='card text-xs md:text-2xl'>
+            <h2>LANDING PAGES</h2>
+          </div>
+
+          <div className='card text-xs md:text-2xl'>
+            <h2>WEB DESIGN</h2>
+          </div>
+
+          <div className='card text-xs md:text-2xl'>
+            <h2>DOMAIN AND HOSTING</h2>
+          </div>
+        </div>
+
+        <Button variant='contained' className='mt-auto'>contact</Button>
+
+      </div>
+
     </Layout>
   )
 }
